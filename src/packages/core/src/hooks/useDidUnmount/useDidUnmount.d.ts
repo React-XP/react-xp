6090ebb2dc5ebe2d43773d5faa0d0bd3a369ffc1
Fábio Maia <fabio.maia@itsector.pt>
2024-06-18 09:18:00 +0100

    feat(global): removed unused files

--- src/packages/core/src/hooks/useDidUnmount/useDidUnmount.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const useDidUnmount: (cb: () => void) => void;
--- src/packages/theme/src/hooks/useTheme/useTheme.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { ThemeContext } from '../../contexts/themeContext';
-export const useTheme = () => {
-  return React.useContext(ThemeContext);
-};
