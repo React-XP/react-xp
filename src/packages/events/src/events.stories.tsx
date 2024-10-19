e5c408e3c1f0dc768502cbdac3c3922862c87f72
Fábio Maia <fabio.maia@itsector.pt>
2024-10-19 16:51:35 +0100

    feat(events): improve docs

--- src/packages/events/src/events.stories.tsx
+++ src/packages/events/src/events.stories.tsx
@@ -9,6 +9,7 @@ type TPayload = { msg: string };
 
 const Emitter = () => {
   const { emit } = useEventEmitter<TPayload>({ key: 'key' });
+  const { emit: emit2 } = useEventEmitter<TPayload>({ key: 'key2' });
 
   return (
     <>
@@ -22,13 +23,24 @@ const Emitter = () => {
         }}
         onClick={() => emit({ msg: 'hello' })}
       >
-        Emit
+        Emit key=key
+      </button>
+      <button
+        type="button"
+        style={{
+          border: '1px solid black',
+          padding: '5px',
+          borderRadius: '5px',
+        }}
+        onClick={() => emit2({ msg: 'hello key 2' })}
+      >
+        Emit key=key2
       </button>
     </>
   );
 };
 
-const Subscriber = () => {
+const Subscriber1 = () => {
   const [payload, setPayload] = React.useState<TPayload>();
   useEventSubscriber<TPayload>({
     key: 'key',
@@ -37,7 +49,28 @@ const Subscriber = () => {
 
   return (
     <>
-      <div>Subscriber</div>
+      <div>Subscriber for key=[key]</div>
+      <br />
+      Event payload: {JSON.stringify(payload)}
+    </>
+  );
+};
+
+const Subscriber2 = () => {
+  const [payload, setPayload] = React.useState<TPayload>();
+  useEventSubscriber<TPayload>({
+    key: 'key',
+    callback: ({ payload }) => setPayload(payload),
+  });
+  useEventSubscriber<TPayload>({
+    key: 'key2',
+    callback: ({ payload }) => setPayload(payload),
+  });
+
+  return (
+    <>
+      <div>Subscriber 2 for keys=[key,key2]</div>
+      <br />
       Event payload: {JSON.stringify(payload)}
     </>
   );
@@ -53,7 +86,14 @@ const Component = () => {
       This is a {'events'} example
       <br />
       <Emitter />
-      <Subscriber />
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ display: 'flex' }}>
+          <Subscriber1 />
+        </div>
+        <div style={{ display: 'flex' }}>
+          <Subscriber2 />
+        </div>
+      </div>
     </EventsProvider>
   );
 };
